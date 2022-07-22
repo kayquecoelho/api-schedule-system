c2bbe8fe65fd00202fbdbb874308233f899a0c76
Kayque <lkcoelho76@gmail.com>
2022-07-22 10:37:36 -0300

    refactor: apply DRY concept to format date

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,8 @@
 import { PrismaClient } from '@prisma/client';
-import dayjs from 'dayjs';
-import utc from 'dayjs/plugin/utc.js';
-import customParseFormat from 'dayjs/plugin/customParseFormat.js';
-dayjs.extend(utc);
-dayjs.extend(customParseFormat)
+import formatDate from '../src/utils/formatDate.js';
 const prisma = new PrismaClient();
 
-async function main() {
+export default async function seedDatabase() {
   const empresaB = await prisma.client.upsert({
     where: {
       cnpj: '00000000002',
@@ -35,7 +31,7 @@ async function main() {
     {
       initialism: '00001CIVELRJ',
       isActive: true,
-      createdAt: dayjs('10-10-2007', 'DD-MM-YYYY').utc().format(),
+      createdAt: formatDate('10-10-2007'),
       charge: 20000000,
       state: 'Rio de Janeiro',
       clientId: empresaA.id,
@@ -43,15 +39,15 @@ async function main() {
     {
       initialism: '00002CIVELSP',
       isActive: true,
-      createdAt: dayjs('20-10-2007', 'DD-MM-YYYY').utc().format(),
+      createdAt: formatDate('20-10-2007'),
       charge: 10000000,
       state: 'São Paulo',
       clientId: empresaA.id,
     },
     {
-      initialism: '00003TRABMJ',
+      initialism: '00003TRABMG',
       isActive: false,
-      createdAt: dayjs('30-10-2007', 'DD-MM-YYYY').utc().format(),
+      createdAt: formatDate('30-10-2007'),
       charge: 1000000,
       state: 'Minas Gerais',
       clientId: empresaA.id,
@@ -59,7 +55,7 @@ async function main() {
     {
       initialism: '00004CIVELRJ',
       isActive: false,
-      createdAt: dayjs('10-11-2007', 'DD-MM-YYYY').utc().format(),
+      createdAt: formatDate('10-11-2007'),
       charge: 2000000,
       state: 'Rio de Janeiro',
       clientId: empresaA.id,
@@ -67,7 +63,7 @@ async function main() {
     {
       initialism: '00005CIVELSP',
       isActive: true,
-      createdAt: dayjs('15-11-2007', 'DD-MM-YYYY').utc().format(),
+      createdAt: formatDate('15-11-2007'),
       charge: 3500000,
       state: 'São Paulo',
       clientId: empresaA.id,
@@ -78,7 +74,7 @@ async function main() {
     {
       initialism: '00006CIVELRJ',
       isActive: true,
-      createdAt: dayjs('01-05-2007', 'DD-MM-YYYY').utc().format(),
+      createdAt: formatDate('01-05-2007'),
       charge: 2000000,
       state: 'Rio de Janeiro',
       clientId: empresaB.id,
@@ -86,7 +82,7 @@ async function main() {
     {
       initialism: '00007CIVELRJ',
       isActive: true,
-      createdAt: dayjs('02-06-2007', 'DD-MM-YYYY').utc().format(),
+      createdAt: formatDate('02-06-2007'),
       charge: 70000000,
       state: 'Rio de Janeiro',
       clientId: empresaB.id,
@@ -94,7 +90,7 @@ async function main() {
     {
       initialism: '00008CIVELSP',
       isActive: false,
-      createdAt: dayjs('03-07-2007', 'DD-MM-YYYY').utc().format(),
+      createdAt: formatDate('03-07-2007'),
       charge: 50000,
       state: 'São Paulo',
       clientId: empresaB.id,
@@ -102,7 +98,7 @@ async function main() {
     {
       initialism: '00009CIVELSP',
       isActive: true,
-      createdAt: dayjs('04-08-2007', 'DD-MM-YYYY').utc().format(),
+      createdAt: formatDate('04-08-2007'),
       charge: 3200000,
       state: 'São Paulo',
       clientId: empresaB.id,
@@ -110,7 +106,7 @@ async function main() {
     {
       initialism: '00010TRABAM',
       isActive: false,
-      createdAt: dayjs('05-09-2007', 'DD-MM-YYYY').utc().format(),
+      createdAt: formatDate('05-09-2007'),
       charge: 100000,
       state: 'Amazonas',
       clientId: empresaB.id,
@@ -124,7 +120,7 @@ async function main() {
   console.log(lawsuits, empresaA, empresaB);
 }
 
-main()
+seedDatabase()
   .catch((e) => {
     console.error(e);
     process.exit(1);
